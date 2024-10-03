d37829ae4505817b1a753f7f1a422435b13bf873
Adarsh <adarshverma0524@gmail.com>
2024-10-04 00:21:35 +0530

    gitingonte

--- app.js
+++ app.js
@@ -281,7 +281,7 @@ app.use((req, res) => {
 });
 
 // Vercel deployment configuration
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
